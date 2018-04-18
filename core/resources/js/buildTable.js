80d0a71a3fe84dabb0a3562ff3f2b88c7151df0d
Andrey Alekseevich <magister.demens@gmail.com>
2018-04-18 18:54:36 +0300

    summary report pre-final version

--- core/resources/js/buildTable.js
+++ core/resources/js/buildTable.js
@@ -2,7 +2,7 @@ function buildTable(tableID) {
 
   $(tableID).dynatable({
     table: {
-        defaultColumnIdStyle: 'trimDash'
+        defaultColumnIdStyle: 'camelCase'
     },
   	features: {
   		paginate: true,
@@ -14,6 +14,6 @@ function buildTable(tableID) {
   var dynatable = $(tableID).data('dynatable');
   dynatable.paginationPerPage.set(15);          // Show 20 records per page
   dynatable.paginationPage.set(1);              // Go to page 5
-  dynatable.sorts().add("testCase", 1);
+  dynatable.sorts.add("testCase", 1);
   dynatable.process();
 }
