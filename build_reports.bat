f018b4371399a5d338eb0a2328cc88392d9d15cf
Andrey Alekseevich <magister.demens@gmail.com>
2018-06-07 21:06:42 +0300

    hotfix

--- build_reports.bat
+++ build_reports.bat
@@ -1,4 +1,3 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 set REPORTS_DIR=%1
-set BRANCH_NAME=%2
 python -c "import core.reportExporter; core.reportExporter.build_summary_reports('%REPORTS_DIR%' '%BRANCH_NAME%')"
