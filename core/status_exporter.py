d3c729a61b37ab898570e1b8431a0497d8adab5f
Andrey Alekseevich <magister.demens@gmail.com>
2018-08-27 20:17:43 +0300

    slack notification style fix added metaAJAX to compare report

--- core/status_exporter.py
+++ core/status_exporter.py
@@ -13,8 +13,9 @@ def main(work_dir=''):
         args.work_dir = work_dir
 
     summary_report = {}
+    max_name = 12
     total = {'total': 0, 'passed': 0, 'failed': 0, 'error': 0, 'skipped': 0, 'duration': 0, 'render_duration': 0}
-    status_to_export = "_Test Machine_ | *total*/passed/`failed`/`error`/skipped\n"
+    status_to_export = ""
 
     if os.path.exists(os.path.join(args.work_dir, SUMMARY_REPORT)):
         with open(os.path.join(args.work_dir, SUMMARY_REPORT), 'r') as file:
@@ -34,6 +35,7 @@ def main(work_dir=''):
                     name_fill=max_name
                 )
 
+    status_to_export = "_{: <{name_fill}}_ | *total*/passed/`failed`/`error`/skipped\n".format("Test Machine", name_fill=max_name) + status_to_export
     # get summary results
     for execution in summary_report:
         for key in total:
