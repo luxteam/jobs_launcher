9d4ad42de381298bcc79912c16c691e3ecafea91
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-12-10 11:28:46 +0300

    Update install_web_set_python3default.sh

--- install/install_web_set_python3default.sh
+++ install/install_web_set_python3default.sh
@@ -1,6 +1,6 @@
 # sudo apt install gcc python-dev python3.5-dev python-pip
-sudo apt install python3-numpy python3-scipy
-# alias python=python3
+# sudo apt install python3-numpy python3-scipy
+apt install gcc python-dev
 
 python get-pip.py
 python -m pip install --upgrade pip wheel setuptools
@@ -8,8 +8,8 @@ python -m pip install --upgrade pip wheel setuptools
 python -m pip install psutil
 
 python -m pip install mkl
-# python -m pip install numpy
-# python -m pip install scipy
+python -m pip install numpy
+python -m pip install scipy
 python -m pip install olefile
 python -m pip install Pillow
 
@@ -19,4 +19,4 @@ python -m pip install Jinja2
 python -m pip install pytest
 python -m pip install pyscreenshot
 # python -m pip install pyopencl
-python -m pip insall py-cpuinfo #--user
+python -m pip install py-cpuinfo #--user
