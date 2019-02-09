ef7357a3cb3386eeca0f8e9f841c19a2fef70eea
Andrey Alekseevich <magister.demens@gmail.com>
2019-02-09 16:59:29 +0300

    refactor baseline comparison process
    (cherry picked from commit c6d438a)

--- common/scripts/ImageComparator/compareByJSON.py
+++ common/scripts/ImageComparator/compareByJSON.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import json
 import CompareMetrics
+import shutil
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, os.path.pardir)))
 import core.config
@@ -17,6 +18,15 @@ def createArgParser():
     return argparser
 
 
+def get_diff(current, previous):
+    if current == previous:
+        return 0.0
+    try:
+        return (current - previous) / previous * 100.0
+    except ZeroDivisionError:
+        return 0
+
+
 def get_pixel_difference(work_dir, base_dir, img, baseline_json, tolerance, pix_diff_max):
 
     for key in core.config.POSSIBLE_JSON_IMG_RENDERED_KEYS:
@@ -24,7 +34,7 @@ def get_pixel_difference(work_dir, base_dir, img, baseline_json, tolerance, pix_
             render_img_path = os.path.join(work_dir, img[key])
 
             try:
-                baseline_img_path = os.path.join(base_dir, baseline_json[img['file_name']])
+                baseline_img_path = os.path.join(base_dir, baseline_json[key])
             except KeyError as err:
                 core.config.main_logger.error("No such file in baseline: {}".format(str(err)))
                 continue
@@ -46,33 +56,17 @@ def get_pixel_difference(work_dir, base_dir, img, baseline_json, tolerance, pix_
                 # TODO: swap status
                 img['test_status'] = 'error'
             img.update({'baseline_color_path': os.path.relpath(
-                os.path.join(base_dir, baseline_json[img['file_name']]), work_dir)})
+                os.path.join(base_dir, baseline_json[key]), work_dir)})
 
     return img
 
 
-def get_rendertime_difference(base_dir, img, time_diff_max):
-    if os.path.exists(os.path.join(base_dir, img['test_group'], core.config.BASELINE_REPORT_NAME)):
-        render_time = img['render_time']
-        with open(os.path.join(base_dir, img['test_group'], core.config.BASELINE_REPORT_NAME), 'r') as file:
-            try:
-                baseline_time = [x for x in json.loads(file.read()) if x['test_case'] == img['test_case']][0]['render_time']
-            except IndexError:
-                baseline_time = -0.0
+def get_rendertime_difference(img, baseline_item, time_diff_max):
+    render_time = img['render_time']
+    baseline_time = baseline_item['render_time']
 
-        def get_diff():
-            if render_time == baseline_time:
-                return 0.0
-            try:
-                return (render_time - baseline_time) / baseline_time * 100.0
-            except ZeroDivisionError:
-                return 0
-
-        img.update({'difference_time': get_diff()})
-        img.update({'baseline_render_time': baseline_time})
-    else:
-        img.update({'difference_time': -0.0})
-        img.update({'baseline_render_time': -0.0})
+    img.update({'difference_time': get_diff(render_time, baseline_time)})
+    img.update({'baseline_render_time': baseline_time})
 
     return img
 
@@ -81,48 +75,40 @@ def main():
     args = createArgParser().parse_args()
 
     render_json_path = os.path.join(args.work_dir, core.config.TEST_REPORT_NAME)
-    baseline_json_path = os.path.join(args.base_dir, core.config.BASELINE_MANIFEST)
 
     if not os.path.exists(render_json_path):
         core.config.main_logger.error("Render report doesn't exists")
-        return
+        return 1
 
-    if not os.path.exists(args.base_dir) or not os.path.exists(baseline_json_path):
+    if not os.path.exists(args.base_dir):
         core.config.main_logger.warning("Baseline or manifest not found by path: {}".format(args.base_dir))
-
-        try:
-            with open(render_json_path, 'r') as file:
-                render_json = json.loads(file.read())
-                for img in render_json:
-                    img['baseline_render_time'] = -0.0
-                    img['difference_time'] = -0.0
-        except Exception as err:
-            core.config.main_logger.error("Can't read report.json: {}".format(str(err)))
-        else:
-            with open(os.path.join(args.work_dir, core.config.TEST_REPORT_NAME_COMPARED), 'w') as file:
-                json.dump(render_json, file, indent=4)
-        finally:
-            return
+        shutil.copyfile(render_json_path, os.path.join(args.work_dir, core.config.TEST_REPORT_NAME_COMPARED))
+        return 1
 
     with open(render_json_path, 'r') as file:
         render_json = json.loads(file.read())
 
-    with open(baseline_json_path, 'r') as file:
-        baseline_json = json.loads(file.read())
-
     for img in render_json:
-        # if failed it means tool crash - no sense to compare images
-        if img['test_status'] != core.config.TEST_CRASH_STATUS:
-            img.update(get_pixel_difference(args.work_dir, args.base_dir, img, baseline_json, args.pix_diff_tolerance, args.pix_diff_max))
-
-            img.update(get_rendertime_difference(args.base_dir, img, args.time_diff_max))
+        baseline_json_path = os.path.join(args.base_dir, img['test_case'] + core.config.CASE_REPORT_SUFFIX)
+        if not os.path.exists(baseline_json_path):
+            core.config.main_logger.error("Test case {} report not found".format(img['test_case']))
+            continue
         else:
-            img['difference_time'] = -0.0
-            img['baseline_render_time'] = -0.0
+            with open(baseline_json_path, 'r') as file:
+                baseline_item = json.loads(file.read())
+
+        get_pixel_difference(args.work_dir, args.base_dir, img, baseline_item[0], args.pix_diff_tolerance, args.pix_diff_max)
+        get_rendertime_difference(img, baseline_item, args.time_diff_max)
+        try:
+            img.update({"baseline_render_device": baseline_item[0]['render_device']})
+        except KeyError:
+            core.config.main_logger.error("Can't get baseline render device")
 
     with open(os.path.join(args.work_dir, core.config.TEST_REPORT_NAME_COMPARED), 'w') as file:
         json.dump(render_json, file, indent=4)
 
+    return 0
+
 
 if __name__ == '__main__':
-    main()
+    exit(main())
--- core/config.py
+++ core/config.py
@@ -6,6 +6,28 @@ logging.basicConfig(filename='launcher.engine.log',
                     format=u'%(filename)-19s[LINE:%(lineno)-3d] #%(levelname)-8s [%(asctime)s] %(message)s')
 main_logger = logging.getLogger('main_logger')
 
+RENDER_REPORT_BASE = {
+    "file_name": "",
+    "date_time": "",
+    "script_info": [],
+    "render_color_path": "",
+    "test_case": "",
+    "render_version": "",
+    "test_status": "undefined",
+    "tool": "",
+    "render_time": -0.0,
+    "baseline_render_time": -0.0,
+    "render_mode": "",
+    "scene_name": "",
+    "test_group": "",
+    "difference_color": "not compared yet",
+    "difference_time": "not compared yet",
+    "core_version": "",
+    "render_device": ""
+}
+RENDER_REPORT_BASE_USEFULL_KEYS = ['tool', 'render_version', 'test_group', 'core_version', 'render_device']
+
+SIMPLE_RENDER_TIMEOUT = 10
 TIMEOUT = 900
 TIMEOUT_PAR = 3
 
@@ -16,19 +38,21 @@ TIME_DIFF_MAX = 5
 TEST_CRASH_STATUS = 'failed'
 TEST_DIFF_STATUS = 'error'
 
+CASE_REPORT_SUFFIX = '_RPR.json'
 TEST_REPORT_NAME = 'report.json'
 TEST_REPORT_NAME_COMPARED = 'report_compare.json'
 TEST_REPORT_EXPECTED_NAME = 'expected.json'
-NOT_RENDERED_REPORT = "not_rendered.json"
 TEST_REPORT_HTML_NAME = 'result.html'
 
 SESSION_REPORT = 'session_report.json'
 SESSION_REPORT_HTML = 'session_report.html'
 
-POSSIBLE_JSON_IMG_KEYS = ['redshift_rendered_path', 'rpr_rendered_path', 'rpr_baseline_path', 'render_color_path', 'baseline_color_path']
+NOT_RENDERED_REPORT = "not_rendered.json"
+
+POSSIBLE_JSON_IMG_KEYS = ['baseline_color_path', 'baseline_opacity_path', 'render_color_path', 'render_opacity_path']
 POSSIBLE_JSON_IMG_KEYS_THUMBNAIL = ['thumb64_' + x for x in POSSIBLE_JSON_IMG_KEYS]
 POSSIBLE_JSON_IMG_KEYS_THUMBNAIL = POSSIBLE_JSON_IMG_KEYS_THUMBNAIL + ['thumb256_' + x for x in POSSIBLE_JSON_IMG_KEYS]
-POSSIBLE_JSON_IMG_RENDERED_KEYS = ['redshift_rendered_path', 'rpr_rendered_path', 'render_color_path', 'baseline_color_path']
+POSSIBLE_JSON_IMG_RENDERED_KEYS = ['render_color_path', 'render_opacity_path']
 POSSIBLE_JSON_IMG_RENDERED_KEYS_THUMBNAIL = ['thumb64_' + x for x in POSSIBLE_JSON_IMG_RENDERED_KEYS]
 POSSIBLE_JSON_IMG_RENDERED_KEYS_THUMBNAIL = POSSIBLE_JSON_IMG_RENDERED_KEYS_THUMBNAIL + ['thumb256_' + x for x in POSSIBLE_JSON_IMG_RENDERED_KEYS]
 
