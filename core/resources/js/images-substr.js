48733ebca9067596f615c13c301995c2ade9323e
Andrey Alekseevich <magister.demens@gmail.com>
2019-05-07 19:56:53 +0300

    Fixed images resizing in popup window

--- core/resources/js/images-substr.js
+++ core/resources/js/images-substr.js
@@ -1,8 +1,7 @@
 function showImagesSubtraction(baselineId, renderId) {
 
-//    TODO: save images proportions while views switching
     if (!($("#baselineImgPopup").attr('src') && $("#renderedImgPopup").attr('src'))) {
-        infoBox("Can't read image", "#815e9b");
+        infoBox("[Error] Can't read source image.", "#815e9b");
         return;
     }
 
@@ -10,39 +9,31 @@ function showImagesSubtraction(baselineId, renderId) {
     var imagesTable = document.getElementById("imgsCompareTable");
     var diffTable = document.getElementById('imgsDiffTable');
 
-    if (diffCanvas && diffTable.style.display == "block") {
+    // if diff image is show now
+    if (diffTable.style.display == "") {
         imagesTable.style.display = "";
         diffTable.style.display = "none";
         return;
-    }
-    else if (diffCanvas && diffTable.style.display == "none") {
-        diffCanvas.parentNode.removeChild(diffCanvas);
     }
 
     var img1 = document.getElementById(baselineId);
     var img2 = document.getElementById(renderId);
 
-    var width = img1.naturalWidth;
-    var height = img1.naturalHeight;
+    diffCanvas.width = img1.naturalWidth;
+    diffCanvas.height = img1.naturalHeight;
 
-    var canvasParent = document.getElementById('diffImageTd');
-    var c = document.createElement('canvas');
-    c.id = "imgsDifferenceCanvas";
-    c.width = width;
-    c.height = height;
+    var ctx = diffCanvas.getContext("2d");
+    ctx.clearRect(0, 0, diffCanvas.width, diffCanvas.height)
 
-    var ctx = c.getContext("2d");
     ctx.drawImage(img1, 0, 0);
-    var imgData1 = ctx.getImageData(0, 0, width, height);
+    var imgData1 = ctx.getImageData(0, 0, diffCanvas.width, diffCanvas.height);
     ctx.drawImage(img2, 0, 0);
-    var imgData2 = ctx.getImageData(0, 0, width, height);
-    var diff = ctx.createImageData(width, height);
+    var imgData2 = ctx.getImageData(0, 0, diffCanvas.width, diffCanvas.height);
 
-    pixelmatch(imgData1.data, imgData2.data, diff.data, width, height, {threshold: 0.03});
-
+    var diff = ctx.createImageData(diffCanvas.width, diffCanvas.height);
+    pixelmatch(imgData1.data, imgData2.data, diff.data, diffCanvas.width, diffCanvas.height, {threshold: 0.03});
     ctx.putImageData(diff, 0, 0);
 
     imagesTable.style.display = "none";
-    diffTable.style.display = "block";
-    canvasParent.appendChild(c);
+    diffTable.style.display = "";
 }
