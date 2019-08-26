39c9692868048cd8b39a5fa64b7770836ed72e32
Andrey Alekseevich <magister.demens@gmail.com>
2019-08-26 18:19:17 +0300

    keys fix

--- core/reportExporter.py
+++ core/reportExporter.py
@@ -335,10 +335,10 @@ def build_local_reports(work_dir, summary_report, common_info):
                     if os.path.exists(os.path.join(work_dir, report_dir, TEST_REPORT_NAME_COMPARED)):
                         with open(os.path.join(work_dir, report_dir, TEST_REPORT_NAME_COMPARED), 'r') as file:
                             render_report = json.loads(file.read())
-                            common_info.update({'tool': render_report[0]['tool']})
-                            common_info.update({'render_device': render_report[0]['render_device']})
-                            common_info.update({'testing_start': render_report[0]['date_time']})
-                            common_info.update({'test_group': render_report[0]['test_group']})
+                            keys_for_upd = ['tool', 'render_device', 'testing_start', 'test_group']
+                            for key_upd in keys_for_upd:
+                                if key_upd in render_report[0].keys():
+                                    common_info.update({key_upd: render_report[0][key_upd]})
 
                     if os.path.exists(baseline_report_path):
                         with open(baseline_report_path, 'r') as file:
