125b046c695d5564bdb6490504dd328cbd153226
egurin <egurin@luxoft.com>
2020-04-20 19:12:23 +0300

    add logger

--- rbs_client.py
+++ rbs_client.py
@@ -1,16 +1,20 @@
 import json
 from requests.auth import HTTPBasicAuth
 from requests import get, post, put
+import logging
+
+logging.basicConfig(filename='rbs.log',level=logging.DEBUG)
+logger = logging.getLogger("rbs")
 
 
-class Client:
+class RBS_Client:
     def __init__(
         self,
         build_id,
         suite_id,
         job_id,
         url,
-        env_label,
+        env_label
     ):
         # TODO: in thread loading schema from api
         self.job_id = job_id
@@ -23,67 +27,78 @@ class Client:
         self.get_token()
 
     def get_token(self):
-        response = post(
-            url="{url}/user/login".format(url=self.url),
-            auth=HTTPBasicAuth('dm1tryG', 'root'),
-        )
+        try:
+            response = post(
+                url="{url}/user/login".format(url=self.url),
+                auth=HTTPBasicAuth('dm1tryG', 'root'),
+            )
 
-        token = json.loads(response.content)["token"]
-        self.token = token
-        self.headers = {
-            "Authorization": "Bearer " + token,
-            "Content-Type": "application/json"
-        }
+            token = json.loads(response.content)["token"]
+            self.token = token
+            self.headers = {
+                "Authorization": "Bearer " + token,
+                "Content-Type": "application/json"
+            }
+        except Exception as e:
+            logger.error(str(e))
+
 
     def get_suite_id_by_name(suite_name):
-        response = get(url="{url}/api/build?id={build_id}&jobId={job_id}".format(
-                url=self.url,
-                build_id=self.build_id,
-                job_id=self.job_id
-            )
-        )
-
         try:
+            response = get(url="{url}/api/build?id={build_id}&jobId={job_id}".format(
+                    url=self.url,
+                    build_id=self.build_id,
+                    job_id=self.job_id
+                )
+            )
+
             suites = [el['suite'] for el in json.loads(response.content)['suites'] if el['suite']['name'] == suite_name]
-            self.suite_id =  suites[0]['_id']
-        except Exception:
+            self.suite_id = suites[0]['_id']
+        except Exception as e:
             self.suite_id = None
+            logger.error(str(e))
 
 
 
     def send_test_suite(self, res, env):
-        data = {
-            "test_cases_results": res,
-            "environment": env,
-            "env_label": self.env_label
-        }
-        response = post(
-            headers=self.headers,
-            data=json.dumps(data),
-            url="{url}/api/testSuiteResult?jobId={job_id}&buildId={build_id}&suiteId={suite_id}".format(
-                url=self.url,
-                build_id=self.build_id,
-                suite_id=self.suite_id,
-                job_id=self.job_id
+        try:
+            data = {
+                "test_cases_results": res,
+                "environment": env,
+                "env_label": self.env_label
+            }
+            response = post(
+                headers=self.headers,
+                data=json.dumps(data),
+                url="{url}/api/testSuiteResult?jobId={job_id}&buildId={build_id}&suiteId={suite_id}".format(
+                    url=self.url,
+                    build_id=self.build_id,
+                    suite_id=self.suite_id,
+                    job_id=self.job_id
+                )
             )
-        )
-        print(response.content)
+            logger.info(response.content)
 
-        return response
+            return response
+        except Exception as e:
+            logger.error(str(e))
 
     def define_environment(self, env):
-        data = {
-            "env_label": self.env_label,
-            "environment": env
-        }
-        response = put(
-            headers=self.headers,
-            data=json.dumps(data),
-            url="{url}/api/testSuiteResult?jobId={job_id}&buildId={build_id}&suiteId={suite_id}".format(
-                url=self.url,
-                build_id=self.build_id,
-                suite_id=self.suite_id,
-                job_id=self.job_id
+        try:
+            data = {
+                "env_label": self.env_label,
+                "environment": env
+            }
+            response = put(
+                headers=self.headers,
+                data=json.dumps(data),
+                url="{url}/api/testSuiteResult?jobId={job_id}&buildId={build_id}&suiteId={suite_id}".format(
+                    url=self.url,
+                    build_id=self.build_id,
+                    suite_id=self.suite_id,
+                    job_id=self.job_id
+                )
             )
-        )
-        return response
+            return response
+        except Exception as e:
+            logger.error(str(e))
