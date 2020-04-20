76ecf4ba88d8f01c6524ae83272434785e8b2d89
egurin <egurin@luxoft.com>
2020-04-20 17:19:10 +0300

    add image service client

---
+++ image_service_client.py
@@ -0,0 +1,33 @@
+import json
+from requests.auth import HTTPBasicAuth
+from requests import get, post, put
+
+
+class ISClient:
+    def __init__(self, url):
+        self.url = url
+        self.get_token()
+
+    def get_token(self):
+        request = post(
+            url="{url}/api/login".format(url=self.url),
+            auth=HTTPBasicAuth('admin', ')m$`mMcd)!u(!X"/'),
+        )
+
+        print(json.loads(request.content))
+        token = json.loads(request.content)["token"]
+        self.token = token
+        self.headers = {
+            "Authorization": "Bearer " + token,
+        }
+
+    def send_image(self, path2img):
+        with open(path2img, 'rb') as img:
+            request = post(
+                url="{url}/api/".format(url=self.url),
+                files={
+                    'image': img
+                },
+                headers=self.headers
+            )
+        return json.loads(request.content)["image_id"]
