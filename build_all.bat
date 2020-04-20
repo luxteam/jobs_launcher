ce9d57eeb289d32a3514053617716b7ccd3847fa
Ilia Nemankov <INemankov@luxoft.com>
2020-04-20 12:53:47 +0200

    Remove unused param from build_all bat and sh scripts

--- build_all.bat
+++ build_all.bat
@@ -1,2 +1,2 @@
-call build_reports.bat %1 %2 %3 %4 %5 %6
+call build_reports.bat %1 %2 %3 %4 %5
 call get_status.bat %1
--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./build_reports.sh $1 $2 $3 $4 $5 $6
+./build_reports.sh $1 $2 $3 $4 $5
 ./get_status.sh $1
