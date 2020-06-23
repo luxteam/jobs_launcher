5a0bfe207cddec70106e2d2c1829de64eac1f886
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-06-23 16:00:40 +0300

    add case name to performance info

--- core/performance_counter.py
+++ core/performance_counter.py
@@ -4,10 +4,10 @@ import json
 from datetime import datetime
 
 
-def event_record(dir, event, start):
+def event_record(dir, event, start, case=''):
     dir = os.path.join(dir, 'events')
     if not os.path.exists(dir):
         os.makedirs(dir)
     event_file_name = str(glob.glob(os.path.join(dir, '*.json')).__len__() + 1) + '.json'
     with open(os.path.join(dir, event_file_name), 'w') as f:
-        f.write(json.dumps({'name': event, 'time': datetime.now().strftime('%d/%m/%Y %H:%M:%S.%f'), 'start': start}, indent=4))
+        f.write(json.dumps({'name': event, 'time': datetime.now().strftime('%d/%m/%Y %H:%M:%S.%f'), 'start': start, 'case': case}, indent=4))
