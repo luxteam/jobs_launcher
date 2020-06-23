0338d72e95f3b8e67b822a5122d84b20c2491685
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-06-23 05:40:43 +0300

    add performance counter

---
+++ core/performance_counter.py
@@ -0,0 +1,13 @@
+import glob
+import os
+import json
+from datetime import datetime
+
+
+def event_record(dir, event, start):
+    dir = os.path.join(dir, 'events')
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    event_file_name = str(glob.glob(os.path.join(dir, '*.json')).__len__() + 1) + '.json'
+    with open(os.path.join(dir, event_file_name), 'w') as f:
+        f.write(json.dumps({'name': event, 'time': datetime.now().strftime('%d/%m/%Y %H:%M:%S'), 'start': start}, indent=4))
