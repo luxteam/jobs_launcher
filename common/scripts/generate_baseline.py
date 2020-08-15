ca72b7260ebb850c8ba88642524b5791c6455033
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-08-15 15:19:10 +0300

    return old files

--- common/scripts/generate_baseline.py
+++ common/scripts/generate_baseline.py
@@ -1,12 +1,13 @@
-import argparse
-import shutil
-import os
-import json
 import sys
+import argparse
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, os.path.pardir)))
+import common.scripts.generate_baseline_default as generate_baseline_default
+import common.scripts.generate_baseline_ct as generate_baseline_ct
+import common.scripts.generate_baseline_ec as generate_baseline_ec
 import core.config
 try:
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, os.path.pardir)))
     from local_config import *
 except ImportError:
     core.config.main_logger.critical("local config file not found. Default values will be used.")
@@ -17,9 +18,8 @@ except ImportError:
 
 def create_args_parser():
     args = argparse.ArgumentParser()
-    args.add_argument('--remove_old', default=False)
-    args.add_argument('--results_root', default='Work\Results')
-    args.add_argument('--baseline_root', default='Work\Baseline')
+    args.add_argument('--results_root')
+    args.add_argument('--baseline_root')
     if report_type == 'ct':
         args.add_argument('--case_suffix', required=False, default=core.config.CASE_REPORT_SUFFIX)
     return args
@@ -31,45 +31,11 @@ if __name__ == '__main__':
 
     args.results_root = os.path.abspath(args.results_root)
     args.baseline_root = os.path.abspath(args.baseline_root)
-
-    report = []
-    if os.path.exists(args.baseline_root) and args.remove_old:
-        shutil.rmtree(args.baseline_root)
-
-    # find and process report_compare.json files
-    for path, dirs, files in os.walk(args.results_root):
-        for file in files:
-            if file == core.config.TEST_REPORT_NAME_COMPARED:
-                # create destination folder in baseline location
-                if args.remove_old:
-                    os.makedirs(os.path.join(args.baseline_root, os.path.relpath(path, args.results_root)))
-                # copy json report with new names
-                with open(os.path.join(path, file)) as f:
-                    cases = json.load(f)
-                for case in cases:
-                    # remove odd fields
-                    case.pop('baseline_render_time', None)
-                    case.pop('baseline_color_path', None)
-                    case.pop('difference_color_2', None)
-                    case.pop('difference_color', None)
-                    case.pop('difference_time', None)
-                    case.pop('test_status', None)
-
-                    with open(os.path.join(args.baseline_root, os.path.relpath(path, args.results_root), case['test_case'] + '.json'), 'w') as f:
-                        f.write(json.dumps(case, indent=4))
-
-                    # copy rendered images and thumbnails
-                    for img in core.config.POSSIBLE_JSON_IMG_RENDERED_KEYS_THUMBNAIL + core.config.POSSIBLE_JSON_IMG_RENDERED_KEYS:
-                        if img in case.keys():
-                            rendered_img_path = os.path.join(path, case[img])
-                            baseline_img_path = os.path.relpath(rendered_img_path, args.results_root)
-
-                            # create folder in first step for current folder
-                            if not os.path.exists(os.path.join(args.baseline_root, os.path.split(baseline_img_path)[0])):
-                                os.makedirs(os.path.join(args.baseline_root, os.path.split(baseline_img_path)[0]))
-
-                            try:
-                                shutil.copyfile(rendered_img_path,
-                                                os.path.join(args.baseline_root, baseline_img_path))
-                            except IOError as err:
-                                core.config.main_logger.warning("Error baseline copy file: {}".format(str(err)))
+    
+    if report_type == 'default':
+        generate_baseline_default.main(args)
+    elif report_type == 'ct':
+        generate_baseline_ct.main(args)
+    elif report_type == 'ec':
+        generate_baseline_ec.main(args)
+    
