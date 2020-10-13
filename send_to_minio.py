1c78815e29d2ad8e627c54d9c6799a00c8d04bdf
Ilia Nemankov <INemankov@luxoft.com>
2020-10-13 11:55:16 +0200

    Fix send_to_minio

--- send_to_minio.py
+++ send_to_minio.py
@@ -16,9 +16,9 @@ def send_to_minio(files_path, pattern):
     for file in files:
         file_path = os.path.join(files_path, file)
         if minio_client_prod:
-            minio_client_prod.upload_file(file_path, create_mc_client(os.getenv("UMS_BUILD_ID_PROD")))
+            minio_client_prod.upload_file(file_path, os.getenv("UMS_BUILD_ID_PROD"))
         if minio_client_dev:
-            minio_client_dev.upload_file(file_path, create_mc_client(os.getenv("UMS_BUILD_ID_DEV")))
+            minio_client_dev.upload_file(file_path, os.getenv("UMS_BUILD_ID_DEV"))
 
 
 if __name__ == '__main__':
