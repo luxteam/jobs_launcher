fbfb3f19d0c7f3f1e2d124abf404c0259603ead5
Ilia Nemankov <INemankov@luxoft.com>
2020-10-13 12:14:32 +0200

    Fix Beta RVII name

--- core/countLostTests.py
+++ core/countLostTests.py
@@ -22,7 +22,7 @@ PLATFORM_CONVERTATIONS = {
 			"AMD_RXVEGA": "Radeon RX Vega",
 			"AMD_RX5700XT": "AMD Radeon RX 5700 XT",
 			"AMD_RadeonVII": "AMD Radeon VII",
-			"AMD_RadeonVII_Beta": "AMD Radeon VII Beta",
+			"AMD_RadeonVII_Beta": "AMD Radeon VII Beta Driver",
 			"NVIDIA_GF1080TI": "GeForce GTX 1080 Ti",
 			"AMD_WX7100": "AMD Radeon (TM) Pro WX 7100 Graphics",
 			"AMD_WX9100": "Radeon (TM) Pro WX 9100",
