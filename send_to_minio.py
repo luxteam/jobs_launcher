ca86c2ff4be62b46dbf71ad3ae051baa93d79c2a
Ilia Nemankov <INemankov@luxoft.com>
2020-10-13 08:57:31 +0200

    Rework send_to_minio (reduce number of env vars)

--- send_to_minio.py
+++ send_to_minio.py
@@ -1,6 +1,5 @@
 import os
 import argparse
-from ums_client import create_ums_client
 from minio_client import create_mc_client
 from core.config import *
 import glob
@@ -8,24 +7,18 @@ import glob
 res = []
 transferred_test_cases = []
 
-ums_client_prod = create_ums_client("PROD")
-ums_client_dev = create_ums_client("DEV")
-minio_client_prod = None
-minio_client_dev = None
-if ums_client_prod:
-    minio_client_prod = create_mc_client(ums_client_prod.job_id)
-if ums_client_dev:
-    minio_client_dev = create_mc_client(ums_client_dev.job_id)
+minio_client_prod = create_mc_client(os.getenv("UMS_JOB_ID_PROD"))
+minio_client_dev = create_mc_client(os.getenv("UMS_JOB_ID_DEV"))
 
 
 def send_to_minio(files_path, pattern):
     files = glob.glob(pattern)
     for file in files:
         file_path = os.path.join(files_path, file)
-        if ums_client_prod and minio_client_prod:
-            minio_client_prod.upload_file(file_path, ums_client_prod.build_id)
-        if minio_client_dev and minio_client_dev:
-            minio_client_dev.upload_file(file_path, ums_client_dev.build_id)
+        if minio_client_prod:
+            minio_client_prod.upload_file(file_path, create_mc_client(os.getenv("UMS_BUILD_ID_PROD"))
+        if minio_client_dev:
+            minio_client_dev.upload_file(file_path, create_mc_client(os.getenv("UMS_BUILD_ID_DEV"))
 
 
 if __name__ == '__main__':
