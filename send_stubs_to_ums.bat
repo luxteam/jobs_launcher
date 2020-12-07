cd3f7b77850bfc89c6e76a3b29349f0650d9b998
Ilia Nemankov <INemankov@luxoft.com>
2020-12-07 19:44:21 +0300

    Fix send_stubs_to_ums sh and bat scripts

--- send_stubs_to_ums.bat
+++ send_stubs_to_ums.bat
@@ -5,4 +5,4 @@ set SKIPPED_CASES_PATH=%1
 set ERROR_CASES_PATH=%2
 set RETRY_INFO_PATH=%3
 
-python core\\isGroupSkipped.py --path_to_skipped_cases %SKIPPED_CASES_PATH% --path_to_error_cases %ERROR_CASES_PATH% --path_to_retry_info %RETRY_INFO_PATH%
+python send_stubs_to_ums.py --path_to_skipped_cases %SKIPPED_CASES_PATH% --path_to_error_cases %ERROR_CASES_PATH% --path_to_retry_info %RETRY_INFO_PATH%
--- send_stubs_to_ums.sh
+++ send_stubs_to_ums.sh
@@ -3,4 +3,4 @@ SKIPPED_CASES_PATH="$1"
 ERROR_CASES_PATH="$2"
 RETRY_INFO_PATH="$3"
 
-python core/isGroupSkipped.py --path_to_skipped_cases $SKIPPED_CASES_PATH --path_to_error_cases $ERROR_CASES_PATH --path_to_retry_info $RETRY_INFO_PATH
+python send_stubs_to_ums.py --path_to_skipped_cases $SKIPPED_CASES_PATH --path_to_error_cases $ERROR_CASES_PATH --path_to_retry_info $RETRY_INFO_PATH
