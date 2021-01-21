84876fdc507d164985045fdb51f0719b9cb1e08f
dm1tryG <galkin.dmitry.485@gmail.com>
2021-01-21 10:59:17 +0300

    fixed res type

--- tests/ums.py
+++ tests/ums.py
@@ -29,20 +29,18 @@ def test_create_client():
         r = test_ums_client.define_environment(env)
         assert r.status_code == 200
 
-        res = {
-            "test_cases_results": [
-                {
-                    "artefacts": {
-                        "rendered_image": 'passed'
-                    },
-                    "status": "passed",
-                    "metrics": {
-                        "render_time": random.uniform(10, 100)
-                    },
-                    "name": str(i)
-                } for i in range(5)
-            ]
-        }
+        res = [
+            {
+                "artefacts": {
+                    "rendered_image": 'passed'
+                },
+                "status": "passed",
+                "metrics": {
+                    "render_time": random.uniform(10, 100)
+                },
+                "name": str(i)
+            } for i in range(5)
+        ]
 
         r = test_ums_client.send_test_suite(res=res, env=env)
         assert r.status_code == 200
